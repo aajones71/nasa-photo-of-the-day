d4d203265a60291ec86f60abcb0a867cefc402fa
Antoinette <create@aplusmedia.info>
2019-10-10 21:44:00 -0400

    changed colors

--- src/components/PicCard.js
+++ src/components/PicCard.js
@@ -1,15 +1,22 @@
 import React from "react";
+// import { start } from "repl";
+import styled from "styled-components";
+
+const Pulse = styled.div`
+    background-color: green;
+    color: blue;
+`;
 
  const PicsCard = props => {
     return (
-        <div className= "astroPics" key = {props.id}>
+        <Pulse className= "astroPics" key = {props.id}>
             <h1>Photo Of The Day</h1>
             <h2>Title: {props.title}</h2>
             <p>Date:{props.date}</p>
             <img src= {props.img} alt="NASA"/>
             <p>Explanation: {props.explanation}</p>
             
-        </div>
+        </Pulse>
     );
 };
 export default PicsCard;
